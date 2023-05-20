63788a2094ce1ce8ae05ecde9a9908b4bc8ddda7
Szandra Bagyanszki <20890361+szelini@users.noreply.github.com>
2023-05-20 21:05:12 +0200

    Refactor code

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Landing Page Project
 
-## Table of Contents
+## Project description
 
-* [Instructions](#instructions)
+Udacity Frontend Nanodegree second project: a multi-section landing page. 
+Dynamically builded navigation, smooth scrolls to sections. 
 
-## Instructions
+## Live demo
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+<https://szelini.github.io/udacity-frontend-landing-page/>
--- js/app.js
+++ js/app.js
@@ -22,80 +22,89 @@
  * Define Global Variables
  * 
 */
-const sections = document.querySelectorAll('section');
-
-
-
+let sections;
+let menuUl = document.querySelector('#navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
+//An element is in viewport, if minimum 50% of height is visible
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (rect.top >= 0 && rect.bottom <= window.innerHeight) ||
+        (rect.top >= 0 && rect.top < window.innerHeight / 2) ||
+        (rect.top < 0 && rect.bottom > window.innerHeight / 2);
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
-// append 4th section
-let sectionFour = document.createElement('section');
-sectionFour.setAttribute('id', 'section4');
-sectionFour.setAttribute('data-nav', 'Section 1');
-let newDiv = document.createElement('div');
-newDiv.classList.add('landing__container');
-let newH2 = document.createElement('h2');
-newH2.innerHTML = 'Section 4';
-let newP1 = document.createElement('p');
-newP1.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
-let newP2 = document.createElement('p');
-newP2.innerHTML = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
-newDiv.appendChild(newH2);
-newDiv.appendChild(newP1);
-newDiv.appendChild(newP2);
-sectionFour.appendChild(newDiv);
-document.querySelector('main').appendChild(sectionFour);
-
+//Adds a new lorem ipsum section with the given number
+function addNewSection(num) {
+    let newSection = document.createElement('section');
+    newSection.setAttribute('id', `section${num}`);
+    newSection.setAttribute('data-nav', `Section ${num}`);
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('landing__container');
+    let newH2 = document.createElement('h2');
+    newH2.innerHTML = `Section ${num}`;
+    let newP1 = document.createElement('p');
+    newP1.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.';
+    let newP2 = document.createElement('p');
+    newP2.innerHTML = 'Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.';
+    newDiv.appendChild(newH2);
+    newDiv.appendChild(newP1);
+    newDiv.appendChild(newP2);
+    newSection.appendChild(newDiv);
+    document.querySelector('main').appendChild(newSection);
+}
 
 
 // build the nav
-let navList = document.querySelector('#navbar__list');
-
-for (let i = 0; i < sections.length; i++) {
-    
-    const sectionElement = sections[i];
-    
-    let navLiElement = document.createElement('li');
-    let navLiAElement = document.createElement('a');
-    navLiAElement.innerText = sectionElement.getAttribute('data-nav');
-    navLiAElement.setAttribute('href', '#' + sectionElement.getAttribute('id'));
-    navLiAElement.classList.add('menu__link');
-    
-    navLiElement.appendChild(navLiAElement);
-    navList.appendChild(navLiElement);
+function buildNavigation() {
+    sections = document.querySelectorAll('section');
+    let navList = document.querySelector('#navbar__list');
+    for (let i = 0; i < sections.length; i++) {
+        const sectionElement = sections[i];
+        let navLiElement = document.createElement('li');
+        let navLiAElement = document.createElement('a');
+        navLiAElement.innerText = sectionElement.getAttribute('data-nav');
+        navLiAElement.setAttribute('href', '#' + sectionElement.getAttribute('id'));
+        navLiAElement.classList.add('menu__link');
+        navLiElement.appendChild(navLiAElement);
+        navList.appendChild(navLiElement);
+    }
 }
 
 // Add class 'active' to section when near top of viewport
-
-
+function addActiveClass() {
+    for (let i = 0; i < sections.length; i++) {
+        if (isInViewport(sections[i])) {
+            if (!sections[i].classList.contains("your-active-class"))
+                sections[i].classList.add("your-active-class");
+        }
+        else {
+            if (sections[i].classList.contains("your-active-class"))
+                sections[i].classList.remove("your-active-class");
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
-let menuUl = document.querySelector('#navbar__list');
-menuUl.addEventListener('click', handleClick);
-
 function handleClick(e) {
     const handlerElement = this;
     const sourceElement = e.target;
     const selector = "li a";
     const closestelement = sourceElement.closest(selector);
-    if(handlerElement.contains(closestelement)) {
+    if (handlerElement.contains(closestelement)) {
         const delegatedelement = closestelement;
         e.preventDefault();
         let whereToScroll = document.querySelector(delegatedelement.getAttribute('href'));
         console.log(whereToScroll);
-        whereToScroll.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });        
+        whereToScroll.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
     }
 }
 
@@ -105,62 +114,16 @@ function handleClick(e) {
  *
 */
 
+window.addEventListener("DOMContentLoaded", () => addNewSection(4));
+
 // Build menu 
+window.addEventListener("DOMContentLoaded", () => buildNavigation());
 
 // Scroll to section on link click
+menuUl.addEventListener('click', handleClick);
 
 // Set sections as active
-
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    console.log(el.getAttribute("id") + " rect top:" + rect.top + " rect bottom: " + rect.bottom + " masgasáh: " + (rect.bottom - rect.top) +  "window innerheight: " + window.innerHeight);
-    
-    let elemLathatoMagassag = 0;
-
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight ) {
-            console.log(el.getAttribute('id') + "1.feltétel igaz");
-            
-            return true;        
-    }
-    // lent lóg ki
-    else if (rect.top >= 0 && rect.top < window.innerHeight / 2 ) {
-        console.log(el.getAttribute('id') + "2.feltétel igaz");
-            return true; 
-     
-    }
-
-    //fent lóg ki
-    else if (rect.top < 0 && rect.bottom > window.innerHeight / 2 ) {
-        console.log(el.getAttribute('id') + "3.feltétel igaz");
-        return true; 
-    }
-
-    else {
-        return false;
-    }
-}
-
-
-
-
-
-document.addEventListener('scroll', function () {
-    
-    for (let i = 0; i < sections.length; i++) {
-        if (isInViewport(sections[i])) {
-            if (!sections[i].classList.contains("your-active-class"))
-            sections[i].classList.add("your-active-class");
-            
-        }
-        else {
-            if (sections[i].classList.contains("your-active-class"))
-            sections[i].classList.remove("your-active-class");
-        }
-    }
-
-}, {
-    passive: true
-});
+document.addEventListener('scroll', () => addActiveClass(), { passive: true });
 
 
 
